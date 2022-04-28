e35b375bd65289bf7112923710c442b643123a3e
Oliver Baylis <oliver.baylis@willistowerswatson.com>
2022-04-28 16:23:56 +0100

    Add hero component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { StoreModule } from "@ngrx/store";
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
-import {StoreModule} from "@ngrx/store";
-import {heroReducer} from "./heroes/reducers/hero-reducer";
+import { CreateComponent } from './heroes/components/create/create.component';
 import { ReadDeleteComponent } from './heroes/components/read-delete/read-delete.component';
+import { heroReducer } from "./heroes/reducers/hero-reducer";
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    ReadDeleteComponent
+    ReadDeleteComponent,
+    CreateComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/heroes/components/create/create.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { AppState } from 'src/app/app.state';
+import { AddHero } from "../../actions/hero-action";
+
+@Component({
+  selector: 'app-create',
+  templateUrl: './create.component.html',
+  styleUrls: ['./create.component.scss']
+})
+export class CreateComponent implements OnInit {
+
+  constructor(private store: Store<AppState>) { }
+
+  ngOnInit(): void {
+  }
+
+  addHero(name: string, description: string) {
+    this.store.dispatch(new AddHero({name: name, description: description}))
+  }
+
+}
